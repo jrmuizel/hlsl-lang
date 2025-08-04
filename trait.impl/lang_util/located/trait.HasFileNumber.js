d9fb1518cdfd6c5ea5b4b171c34203272ebfcde0
jrmuizel <jrmuizel@users.noreply.github.com>
2025-08-04 01:47:11 +0000

    deploy: 9d0dd327cf8addc3c373154f1471a002af01f37a

--- trait.impl/lang_util/located/trait.HasFileNumber.js
+++ trait.impl/lang_util/located/trait.HasFileNumber.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["hlsl_lang_pp",[["impl <a class=\"trait\" href=\"lang_util/located/trait.HasFileNumber.html\" title=\"trait lang_util::located::HasFileNumber\">HasFileNumber</a> for <a class=\"struct\" href=\"hlsl_lang_pp/processor/expand/struct.ExpandLocation.html\" title=\"struct hlsl_lang_pp::processor::expand::ExpandLocation\">ExpandLocation</a>"]]]]);
+    var implementors = Object.fromEntries([["hlsl_lang_pp",[["impl HasFileNumber for <a class=\"struct\" href=\"hlsl_lang_pp/processor/expand/struct.ExpandLocation.html\" title=\"struct hlsl_lang_pp::processor::expand::ExpandLocation\">ExpandLocation</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[340]}
+//{"start":57,"fragment_lengths":[215]}
